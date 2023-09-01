9d9322e85bc9a4bc214b95e8e5c6bc49a840d61f
haider-ali-1 <haider.ali.developer123@gmail.com>
2023-09-01 17:20:05 +0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import 'dotenv/config';
 import { app } from './app.js';
-import { connectWithDatabase } from './db/connect.js';
 import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
